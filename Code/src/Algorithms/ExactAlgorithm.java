17e8c4d192956c2d174739b6a1942ecf21044cc2
Brian <brian.dietrich@u-bordeaux.fr>
2020-03-10 12:12:44 +0100

    Exact Algorithm got the good combinaison  but got redondance into it ! still need to optimize it !

--- Code/src/Algorithms/ExactAlgorithm.java
+++ Code/src/Algorithms/ExactAlgorithm.java
@@ -7,14 +7,13 @@ import io.jbotsim.core.Link;
 import io.jbotsim.core.Node;
 import io.jbotsim.core.Topology;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ExactAlgorithm implements AlgorithmModel {
     private Topology tp;
     public static ArrayList<ArrayList<Integer>> combinations= new ArrayList<ArrayList<Integer>> ();
-    public static ArrayList<Integer> candidatsNodes= new ArrayList<Integer> ();
+    public static ArrayList<Integer> candidatsNodes = new ArrayList<Integer>();
+    static List<List<Integer>> powerSet = new LinkedList<List<Integer>>();
     public static int converterToPlace;
 
     public ExactAlgorithm(Topology tp) { this.tp = tp;
@@ -56,6 +55,7 @@ public class ExactAlgorithm implements AlgorithmModel {
         }
         if (k == arr.size() -1){
             combinations.add(tmp);
+           // System.out.println(combinations.toString());
         }
     }
 
@@ -75,50 +75,97 @@ public class ExactAlgorithm implements AlgorithmModel {
         return nodes;
     }
 
+    static void CombinationRepetitionUtil(ArrayList<Integer> chosen, ArrayList<Integer> arr,
+                                          int index, int r, int start, int end) {
+        // Since index has become r, current combination is
+        // ready to be printed, print
+        if (index == r) {
+            ArrayList<Integer> id = new ArrayList<>();
+            for (int i = 0; i < r; i++) {
+                id.add(arr.get(chosen.get(i)));
+            }
+            combinations.add(id);
+            return;
+        }
+
+        // One by one choose all elements (without considering
+        // the fact whether element is already chosen or not)
+        // and recur
+        for (int i = start; i <= end; i++) {
+            chosen.set(index,i);
+            CombinationRepetitionUtil(chosen, arr, index + 1,
+                    r, i, end);
+        }
+        return;
+    }
+    static void CombinationRepetition(ArrayList <Integer> list, int n, int r) {
+        // Allocate memory
+        ArrayList<Integer> chosen = new ArrayList<>();
+        for(int i=0;i<list.size();i++){
+            chosen.add(0);
+        }
+
+        // Call the recursice function
+        CombinationRepetitionUtil(chosen, list, 0, r, 0, n - 1);
+    }
+
+
     public int algorithm() {
+
+
         List <Node> nodes = NodeCandidates();
         for (int i=0;i<nodes.size();i++){
             candidatsNodes.add(nodes.get(i).getID());
         }
 
-        //System.out.println(candidatsNodes.size());
-
-        permute(this.candidatsNodes,0);
-
-       /* for(ArrayList<Integer> permutation: this.combinations){
-           System.out.println(permutation.toString());
+        for(int k=1;k<=candidatsNodes.size();k++){
+            CombinationRepetition(candidatsNodes, candidatsNodes.size(), k);
         }
-        System.out.println(combinations.size());*/
+
+        //permute(this.candidatsNodes,2);
+        /*for(int k=1;k<=candidatsNodes.size();k++){
+            this.powerSet.addAll(combination(candidatsNodes,k));}
+        System.out.println(this.powerSet.toString());*/
+
+        /*for(ArrayList<Integer> permutation: this.combinations){
+           System.out.println(permutation.toString());
+        }*/
         for(int converter=1;converter<nodes.size();converter++){
             int converterTo=converter;
             //System.out.println(converterTo+ " nombre de converter a placer");
             for(List <Integer> permutate : combinations){
                 for(int object=0;object<converterTo;object++){
-                    //System.out.println(converterTo+ " nombre de converter a placer");
-                    Node n = tp.getNodes().get(permutate.get(object));
-                    Router r = (Router)n;
-                    r.addConverter();
-                }
-                if(canCommunicate(tp)) {
-                    //System.out.println(converterTo + "nombre de convertisseur a placer \n ");
-                    System.out.println("J ai reussi le placement avec la combinaison suivante\n");
-                    System.out.println(permutate.toString());
-                    for(Node n : nodes){
-                        Router r=(Router)n;
-                        if (r.hasConverter()){
-                            System.out.println("le routeur"+ r.getID() + " a un convertisseur");
+
+                        //System.out.println(converterTo+ " nombre de converter a placer");
+                        System.out.println(permutate);
+                        Node n = tp.getNodes().get(permutate.get(object));
+                        Router r = (Router) n;
+                        r.addConverter();
+
+                    }
+                    if(canCommunicate(tp)) {
+                        //System.out.println(converterTo + "nombre de convertisseur a placer \n ");
+                        //System.out.println("J ai reussi le placement avec la combinaison suivante\n");
+                       // System.out.println(permutate.toString());
+                        for (Node n : nodes) {
+                            Router r = (Router) n;
+                            /*if (r.hasConverter()) {
+                                System.out.println("le routeur" + r.getID() + " a un convertisseur");
+                            }*/
                         }
+                        return 0;
                     }
-                    return 0;
-                }
-                else{
-                    for(Node n : nodes){
-                        Router r = (Router)n;
-                        r.setConverter();
-                    }
+                    else {
+                        for (Node n : nodes) {
+                            Router r = (Router) n;
+                            r.setConverter();
+                        }
+
                 }
             }
         }
         return 0;
     }
 }
+
+
