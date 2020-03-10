716b059ba12ea2ae7101a900cb4e9c04fea4bb0e
bdietric <brian.dietrich@u-bordeaux.fr>
2020-03-10 16:04:39 +0100

    ExactAlgorithm works !

--- Code/src/Algorithms/ExactAlgorithm.java
+++ Code/src/Algorithms/ExactAlgorithm.java
@@ -9,71 +9,15 @@ import io.jbotsim.core.Topology;
 
 import java.util.*;
 
-public class ExactAlgorithm implements AlgorithmModel {
-    private Topology tp;
+public class ExactAlgorithm extends AlgorithmNonDistributed {
     public static ArrayList<ArrayList<Integer>> combinations= new ArrayList<ArrayList<Integer>> ();
     public static ArrayList<Integer> candidatsNodes = new ArrayList<Integer>();
-    static List<List<Integer>> powerSet = new LinkedList<List<Integer>>();
     public static int converterToPlace;
 
-    public ExactAlgorithm(Topology tp) { this.tp = tp;
+    public ExactAlgorithm(Topology tp) { super(tp);
         ArrayList<ArrayList<Integer>> combinations = new ArrayList<ArrayList<Integer>> ();
         ArrayList<Integer> candidatsNodes = new ArrayList<Integer> ();}
 
-    public boolean canCommunicate(Topology tp) {
-        List<Node> nodeCanCommunicate = new ArrayList<>();
-        Node actual = tp.getNodes().get(0);
-        nodeCanCommunicate.add(actual);
-        List<Node> nodes = tp.getNodes();
-        nodes.remove(actual);
-        List<Node> canCommunicate = canCommunicateRecursive(tp, actual, nodeCanCommunicate,nodes);
-        return canCommunicate.size() == tp.getNodes().size();
-    }
-
-    public List<Node> canCommunicateRecursive(Topology tp, Node a, List<Node> nodeCanCommunicate,List<Node> nodes) {
-        Router actual = (Router) a;
-        for (Node n : a.getNeighbors()) {
-            if (nodes.contains(n)) {
-                Router neighbor = (Router) n;
-                if (neighbor.getClass().equals(actual) || neighbor.hasConverter() || actual.hasConverter()) {
-                    nodeCanCommunicate.add(neighbor);
-                    nodes.remove(n);
-                    return canCommunicateRecursive(tp, neighbor, nodeCanCommunicate,nodes);
-                }
-            }
-        }
-        return nodeCanCommunicate;
-    }
-
-    //:COMMENT : Calculate all the combinaisons of nodes
-    public static void permute(ArrayList<Integer> arr, int k){
-        ArrayList<Integer>  tmp = (ArrayList<Integer>) arr.clone();
-        for(int i = k; i < arr.size(); i++){
-            java.util.Collections.swap(tmp, i, k);
-            permute(tmp, k+1);
-            java.util.Collections.swap(tmp, k, i);
-        }
-        if (k == arr.size() -1){
-            combinations.add(tmp);
-            // System.out.println(combinations.toString());
-        }
-    }
-
-    public List <Node> NodeCandidates(){
-        List<Node> nodes = new ArrayList<>();
-        for(Link l : tp.getLinks()){
-            Router source = (Router)l.source;
-            Router destination= (Router)l.destination;
-            if(source instanceof RouterIPv4 && destination instanceof RouterIPv6
-                    ||source instanceof RouterIPv6 && destination instanceof RouterIPv4 ){
-                if(!nodes.contains(source))
-                    nodes.add(l.source);
-                if (!nodes.contains(destination))
-                    nodes.add(l.destination);
-            }
-        }
-        return nodes;
-    }
 
     static void CombinationRepetitionUtil(ArrayList<Integer> chosen, ArrayList<Integer> arr,
                                           int index, int r, int start, int end) {
@@ -84,7 +28,15 @@ public class ExactAlgorithm implements AlgorithmModel {
             for (int i = 0; i < r; i++) {
                 id.add(arr.get(chosen.get(i)));
             }
-            combinations.add(id);
+            boolean redondance=false;
+            for(int i=0;i<id.size();i++){
+                if(id.size()>1 && i < id.size()-1) {
+                    if (id.get(i)==id.get(i+1)){
+                        redondance=true;
+                    }
+                }
+            }
+            if (!redondance){ combinations.add(id);}
             return;
         }
 
@@ -98,71 +50,48 @@ public class ExactAlgorithm implements AlgorithmModel {
         }
         return;
     }
+
     static void CombinationRepetition(ArrayList <Integer> list, int n, int r) {
         // Allocate memory
         ArrayList<Integer> chosen = new ArrayList<>();
         for(int i=0;i<list.size();i++){
             chosen.add(0);
         }
-
         // Call the recursice function
         CombinationRepetitionUtil(chosen, list, 0, r, 0, n - 1);
     }
 
 
     public int algorithm() {
-
-
         List <Node> nodes = NodeCandidates();
         for (int i=0;i<nodes.size();i++){
             candidatsNodes.add(nodes.get(i).getID());
         }
+        setConnectedComponents(getConnectedComponents(getTopology()));
+        defineConverterToPlace();
 
-        for(int k=1;k<=candidatsNodes.size();k++){
+        for(int k=1;k<= getNbConverterToplace();k++){
             CombinationRepetition(candidatsNodes, candidatsNodes.size(), k);
         }
+        //System.out.println(combinations.toString());
 
-        //permute(this.candidatsNodes,2);
-        /*for(int k=1;k<=candidatsNodes.size();k++){
-            this.powerSet.addAll(combination(candidatsNodes,k));}
-        System.out.println(this.powerSet.toString());*/
-
-        /*for(ArrayList<Integer> permutation: this.combinations){
-           System.out.println(permutation.toString());
-        }*/
-
-            //System.out.println(converterTo+ " nombre de converter a placer");
-            for(List <Integer> permutate : combinations){
-                for(int object=0;object<permutate.size();object++){
-
-                    //System.out.println(converterTo+ " nombre de converter a placer");
-                    System.out.println(permutate);
-                    Node n = tp.getNodes().get(permutate.get(object));
-                    Router r = (Router) n;
-                    r.addConverter();
-
-                }
-                if(canCommunicate(tp)) {
-                    //System.out.println(converterTo + "nombre de convertisseur a placer \n ");
-                    //System.out.println("J ai reussi le placement avec la combinaison suivante\n");
-                    // System.out.println(permutate.toString());
-                    for (Node n : nodes) {
-                        Router r = (Router) n;
-                            /*if (r.hasConverter()) {
-                                System.out.println("le routeur" + r.getID() + " a un convertisseur");
-                            }*/
-                    }
-                    return 0;
-                }
-                else {
-                    for (Node n : nodes) {
-                        Router r = (Router) n;
-                        r.setConverter();
-                    }
-
-                }
+        for(List <Integer> permutate : combinations){
+            for(int object=0;object<permutate.size();object++){
+                Node n = this.getTopology().getNodes().get(permutate.get(object));
+                Router r = (Router) n;
+                placeConverterOnRouter(getTopology(),r);
             }
+            if(this.getComponent().size()==1) {
+                return 0;
+            }
+            else {
+                for (Node n : nodes) {
+                    Router r = (Router) n;
+                    r.setConverter();
+                }
 
+            }
+        }
         return 0;
     }
 }
--- Code/src/Algorithms/GloutonAlgorithm.java
+++ Code/src/Algorithms/GloutonAlgorithm.java
@@ -12,111 +12,17 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GloutonAlgorithm implements AlgorithmModel {
+public class GloutonAlgorithm extends AlgorithmNonDistributed {
 
-    private Topology tp;
-    private ArrayList<ConnectedComponent> component = new ArrayList<>();
+
     private ArrayList<Link> candidatesLinks= new ArrayList<>();
 
-    public GloutonAlgorithm(Topology tp){ this.tp=tp; }
+    public GloutonAlgorithm(Topology tp){ super(tp);}
 
-    public void addComponents(ConnectedComponent c){ component.add(c); }
-
-    public void setConnectedComponents(ArrayList<ConnectedComponent> l){
-        component= l;
-    }
-
-    public void resetConnectedComponents(Topology tp){
-        for(Node n: tp.getNodes()){
-            Router r = (Router)n;
-            r.setComponent(null);
-        }
-    }
-
-    public ArrayList<ConnectedComponent> getConnectedComponents(Topology tp){
-        System.out.println();
-        System.out.println("Lien candidats :" + candidatesLinks.size());
-        for(Link l : candidatesLinks)
-            System.out.println("Source : " + l.source.getID() + ", Destination : " + l.destination.getID());
-        System.out.println();
-        ArrayList<ConnectedComponent> components = new ArrayList<>();
-        List<Node> nodes = tp.getNodes();
-        while(!nodes.isEmpty()){
-            Router r1 = (Router) nodes.get(0);
-            nodes.remove(r1);
-            ConnectedComponent cc1= new ConnectedComponent();
-            ArrayList<Router> connected = new ArrayList<>();
-            connected.add(r1);
-            ArrayList<Router> result = recursiveSameClassNeighbor(connected, r1, nodes);
-            for(Router router : result)
-                cc1.addRouter(router);
-            components.add(cc1);
-        }
-        return components;
-
-    }
-
-    public ArrayList<Router> recursiveSameClassNeighbor(ArrayList<Router> connected, Router previous, List<Node> nodes) {
-        List<Node> neighbors = previous.getNeighbors();
-        for(Router router : connected) {
-            if(neighbors.contains(router)) {
-                neighbors.remove(router);
-            }
-        }
-        for (Node n : neighbors){
-            Router actual = (Router) n;
-            if (previous.getClass().equals(actual.getClass()) || actual.hasConverter() || previous.hasConverter()){
-                if(!connected.contains(actual)) {
-                    connected.add(actual);
-                    recursiveSameClassNeighbor(connected, actual, nodes);
-                }
-                nodes.remove(n);
-            }
-        }
-        return connected;
-    }
-
-    //:COMMENT : Increment the variables candidateLinkNumber of each router if it has a candidateLink
-    public void countCandidatesLink(Topology tp) {
-        for (Node n : tp.getNodes()) {
-            int cpt=0;
-            Router r=(Router)n;
-            for (Link l : candidatesLinks) {
-                if (l.source.equals(n) || l.destination.equals(n)) {
-                    cpt++;
-                }
-            }
-            r.setCandidateLinkNumberCandidate(cpt);
-        }
-    }
-
-    public void candidatLink(Topology tp) {
-        for (Link l : tp.getLinks()) {
-            Router source = (Router) l.source;
-            Router destination = (Router) l.destination;
-            boolean containInComponent = false;
-            if (!(source.getClass().equals(destination.getClass()) || source.hasConverter() || destination.hasConverter())) {
-                for(ConnectedComponent cc : component){
-                    if(cc.contains(source) && cc.contains(destination))
-                    {
-                        containInComponent = true;
-                    }
-                }
-                if(!containInComponent)
-                    candidatesLinks.add(l);
-            }
-        }
-        countCandidatesLink(tp);
-    }
-
-    //:COMMENT : Remove candidates links of a router r
-    public void removeLinkCandidates(Topology tp,Router r){
-        candidatesLinks.clear();
-        countCandidatesLink(tp);
-    }
 
     //:COMMENT : choose the router with the most candidate links
     public Node chooseDegrees(){
+        Topology tp= getTopology();
         Router tmp=(Router)tp.getNodes().get(0);
         for (Node n : tp.getNodes()){
             Router r = (Router)n;
@@ -127,43 +33,11 @@ public class GloutonAlgorithm implements AlgorithmModel {
         return tmp;
     }
 
-    //:COMMENT : Stop condition, if still a router which can becomes a converter
-    public boolean stillaRouterToChoose(){
-        for(Node n : tp.getNodes()){
-            Router r=(Router)n;
-            if(r.getCandidateLinkNumber()>0){
-                return true;
-            }
-        }
-        return false;
-    }
-
     public void placeConverterV1(Topology tp){
         Router r=(Router)chooseDegrees();
-        System.out.println(r.getID() +" " );
         r.addConverter();
         r.resetCandidateLinkNumber();
         removeLinkCandidates(tp,r);
-        System.out.println("V1");
-        for (ConnectedComponent cc : component){
-            System.out.println(cc.toString());
-        }
-        System.out.println();
-        resetConnectedComponents(tp);
-        setConnectedComponents(getConnectedComponents(tp));
-        candidatLink(tp);
-    }
-
-    public void placeConverterLeaf(Topology tp,Router r){
-        System.out.println(r.getID() +" " );
-        r.addConverter();
-        r.resetCandidateLinkNumber();
-        removeLinkCandidates(tp,r);
-        System.out.println("Leaf");
-        for (ConnectedComponent cc : component){
-            System.out.println(cc.toString());
-        }
-        System.out.println();
         resetConnectedComponents(tp);
         setConnectedComponents(getConnectedComponents(tp));
         candidatLink(tp);
@@ -171,18 +45,19 @@ public class GloutonAlgorithm implements AlgorithmModel {
 
 
     public void algorithm(){
+        Topology tp= getTopology();
         candidatLink(tp);
         setConnectedComponents(getConnectedComponents(tp));
         for(Node n: tp.getNodes()){
             if(n.getNeighbors().size()==1){
                 Router u=(Router)n;
                 Router v=(Router)n.getNeighbors().get(0);
-                if(u instanceof RouterIPv6 && v instanceof RouterIPv4 || u instanceof RouterIPv4 && v instanceof RouterIPv6 ){
-                    placeConverterLeaf(tp,v);
+                if(!u.getClass().equals(v.getClass())){
+                    placeConverterOnRouter(tp,v);
                 }
             }
         }
-        while(!candidatesLinks.isEmpty()){
+        while(getComponent().size()>1){
             placeConverterV1(tp);
         }
     }
